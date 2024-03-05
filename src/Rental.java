fd163e20a346bcbb0f512bd7ecc493d81e970760
Mark Abdilla Mifsud <60700422+MarkAbdillaMifsud@users.noreply.github.com>
2024-03-05 21:15:27 +0100

    Rental Class
    Initial implementation

---
+++ src/Rental.java
@@ -0,0 +1,45 @@
+import java.time.LocalDate;
+
+public class Rental {
+    private Property property;
+    private LocalDate rentalStartDate;
+    private LocalDate rentalEndDate;
+
+    public Rental(Property property, LocalDate rentalStartDate, LocalDate rentalEndDate){
+        this.property = property;
+        this.rentalStartDate = rentalStartDate;
+        this.rentalEndDate = rentalEndDate;
+    }
+
+    public Property getProperty() {
+        return property;
+    }
+
+    public void setProperty(Property property) {
+        this.property = property;
+    }
+
+    public LocalDate getRentalStartDate() {
+        return rentalStartDate;
+    }
+
+    public void setRentalStartDate(LocalDate rentalStartDate) {
+        if(rentalStartDate.isAfter(this.rentalEndDate) || rentalStartDate.equals(this.rentalEndDate)){
+            System.out.println("Date of rental start must be before the rental end date");
+        } else {
+            this.rentalStartDate = rentalStartDate;
+        }
+    }
+
+    public LocalDate getRentalEndDate() {
+        return rentalEndDate;
+    }
+
+    public void setRentalEndDate(LocalDate rentalEndDate) {
+        if(rentalEndDate.isBefore(this.rentalStartDate) || rentalEndDate.equals(this.rentalStartDate)){
+            System.out.println("Date of rental end must be after the rental start date");
+        } else {
+            this.rentalEndDate = rentalEndDate;
+        }
+    }
+}
